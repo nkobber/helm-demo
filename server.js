096bf0691581f07d33672edf3e24b080067166b7
Nicolai Kobber <nrk@boligportal.dk>
2018-02-24 23:16:56 +0100

    Updated hello text

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, Pull request!'));
+app.get('/', (req, res) => res.send('Hello, World!'));
 app.get('/ping', (req, res) => res.send('pong'));
 
 app.listen(8080, () => console.error('Listening on port 8080'));
