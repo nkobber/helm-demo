7bfa62e64a3e5af15bc46bdc7e3d6fab6bc0f46a
Nicolai Kobber <nrk@boligportal.dk>
2018-02-26 13:10:11 +0100

    Updated stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.get('/', (req, res) => res.send('Hello, Cloud Native!'));
 app.get('/ping', (req, res) => res.send('pong'));
 
 app.listen(8080, () => console.error('Listening on port 8080'));
